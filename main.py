2a9c4824c62b468b3bb286d0615ef66d65955a55
Brunoo7 <brunoolivera072@gmail.com>
2026-08-10 09:44:14 -0300

    classificação de idade

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ if num % 2 == 0:
 else:
     print(f"O número {num} é ímpar.")
 
+idade = int(input("Insira sua idade: "))
+if 0<= idade <= 12:
+    print("Você é uma criança.")
+
+elif 13 <= idade <= 18:
+    print("Você é um adolescente.")
+
+else:
+    print("Você é um adulto.")
